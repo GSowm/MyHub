10372bf2451dce107866bb83344a7625df17d665
GSowm <sowmya.kannan123@gmail.com>
2026-04-29 12:27:26 +0530

    task 123

---
+++ Python/one.py
@@ -0,0 +1,23 @@
+enames = ("RG","SG","PG");
+print(enames);
+
+enames1 = ["RG","SG","PG"];
+enames1[0] = "Rahul";
+print(enames1);
+
+Eids = {123,234,345}
+Eids.add(888);
+print(Eids);
+
+Emp ={
+    'eid':101,
+    'ename':"Sonia",
+    'esal':45000,
+    'esal':55000
+}
+
+Emp['eid'] = 102;
+print(Emp);
+
+S1 = {};
+print(type(S1));
---
+++ Python/two.py
@@ -0,0 +1,15 @@
+emp={
+    'eid':101,
+    'ename':"Rahul",
+    'esal':45000.45
+}
+
+print(emp);
+print(['eid']);
+print(['ename']);
+print('esal');
+
+print(emp.get('eid'));
+print(emp.get('ename'));
+print(emp.get('esal'));
+print(emp.get('loc'));
